34c786e5d85e72da848a3995054a327a257ae434
Albert <79124590840@ya.ru>
2022-06-30 23:15:08 +0300

    init: initial repo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+    
+    let strNumber = number + '';
+    let result = 0;
+
+    if (strNumber.length <= 2 ) {
+        switch (number) {
+            
+        }
+    }
 }
