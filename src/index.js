7fe21c56163ebd58fad181de18f8850290f616c4
hairhelgen <sultan.albert@yandex.ru>
2022-08-02 23:06:18 +0400

    edit: add condition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 module.exports = function toReadable (number) {
     
-    let strNumber = number + '';
-    let result = 0;
+  let strNumber = number + '';
+  let result = '';
 
-    if (strNumber.length <= 2 ) {
-        switch (number) {
-            
-        }
+  const numberArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'];
+  const twentys = 'teen';
+  // const hundreds = 'hundred';
+  
+  if(strNumber.length === 3) {
+    result += numberArr[+strNumber[0]] + 'hundred';
+    strNumber = strNumber.slice(0);
+  }
+
+  if(+strNumber > 15) {
+    if(strNumber.length === 2) {
+      result += numberArr[+strNumber[0]] + 'teen';
+      strNumber = strNumber.slice(0);
     }
+  } else result += 
 }
