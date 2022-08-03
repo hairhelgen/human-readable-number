18397d19eabae012aa2e058c037e84e12d108f87
hairhelgen <sultan.albert@yandex.ru>
2022-08-03 23:52:20 +0400

    edit: add function of twentys and hundred

--- src/index.js
+++ src/index.js
@@ -3,19 +3,80 @@ module.exports = function toReadable (number) {
   let strNumber = number + '';
   let result = '';
 
-  const numberArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'];
-  const twentys = 'teen';
-  // const hundreds = 'hundred';
-  
-  if(strNumber.length === 3) {
-    result += numberArr[+strNumber[0]] + 'hundred';
-    strNumber = strNumber.slice(0);
+  const numberArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+  const exceptionsNumberArr = {
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    80: 'eighty'
   }
 
-  if(+strNumber > 15) {
-    if(strNumber.length === 2) {
-      result += numberArr[+strNumber[0]] + 'teen';
-      strNumber = strNumber.slice(0);
+  function getException(num) {
+    let res = '';
+
+    if(num < 11) {
+      res = numberArr[num];
+      return res;
+    } else if (exceptionsNumberArr[num]) {
+      res = exceptionsNumberArr[num];
+      return res;
     }
-  } else result += 
+  }
+
+  function getTeens(num) {
+    let res = '';
+    let numStr = num + '';
+
+    res = numberArr[+numStr[1]] + 'teen';
+    return res;
+  }
+
+  function getTwentys(num) {
+    let res = '';
+    let numStr = num + '';
+    let dec = getException(+(numStr[0] + '0'))
+
+    if (dec && +numStr[1] === 0) {
+      return res = dec;
+    } else if (dec && +numStr[1] !== 0) {
+      res = dec + ' ' + numberArr[+numStr[1]];
+      return res;
+    } else if (!(num % 10)) {
+      res = numberArr[+numStr[0]] + 'ty';
+      return res;
+    } else {
+      res = numberArr[+numStr[0]] + 'ty' + ' ' + numberArr[+numStr[1]];
+      return res;
+    }
+  }
+
+  function getHundred(num) {
+    let res = '';
+    let numStr = num + '';
+    
+    res = numberArr[+numStr[0]] + ' hundred'
+
+    if(!(+numStr % 10) && +numStr[1] === 0) {
+      return res;
+    }
+    numStr = numStr.slice(1);
+
+    return res += ' ' + getTwentys(+numStr);
+  }
+
+  if(getException(number)) {
+    return getException(number);
+  } else if (number < 20) {
+    return getTeens(number);
+  } else if (number < 100) {
+    return getTwentys(number);
+  } else return getHundred(number);
+
+  return result;
 }
